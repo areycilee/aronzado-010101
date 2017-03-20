681457a8b08aa778fef640945f6d46c3841ae967
areycilee <aronzadoreycilemae@gmail.com>
2017-03-20 14:43:32 +0800

    My first commit

--- server.js
+++ server.js
@@ -6,23 +6,23 @@ var app = express();
 //set port
 app.set('port', (process.env.PORT || 5000));
 //use static files
-app.use(express.static(path.join(__dirname, 'public')));
+	app.use(express.static(path.join(__dirname, 'public')));
 //express routes
-app.get('/', function(req, res){
+ app.get('/', function(req, res){
   res.sendFile(path.join(__dirname, 'views/home.html'));
-});
-app.get('/app', function(req, res){
+     });
+	app.get('/app', function(req, res){
   res.sendFile(path.join(__dirname, 'views/app.html'));
-});
-app.get('/app2', function(req, res){
-  res.sendFile(path.join(__dirname, 'views/app2.html'));
-});
-app.get('/app3', function(req, res){
-  res.sendFile(path.join(__dirname, 'views/app3.html'));
-});
-app.get('/home', function(req, res){
-  res.sendFile(path.join(__dirname, 'views/home.html'));
-
+  });
+	app.get('/app2', function(req, res){
+ res.sendFile(path.join(__dirname, 'views/app2.html'));
+  });
+	app.get('/app3', function(req, res){
+    res.sendFile(path.join(__dirname, 'views/app3.html'));
+  });
+	app.get('/home', function(req, res){
+res.sendFile(path.join(__dirname, 'views/home.html'));
+ });
 //express server listen
 var server = app.listen(app.get('port'), function(){
   console.log('Server listening on port ',app.get('port'));
